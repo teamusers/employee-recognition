23cfea4db0484f96169478db4bf436a1cf7bd220
LorenzKyle <lorenzkyle.vi@user.com.sg>
2022-10-07 16:52:48 +0800

    Added current and past quarter

--- api/controllers/parser.controller.js
+++ api/controllers/parser.controller.js
@@ -10,11 +10,14 @@ class parser {
         try{
             let kpi = req.query.kpi
             let file = await formatter.scorecardToJSON('C:\\Users\\USERPH\\Desktop\\scorecard-summary-export.xlsx')
-            let parsedData = await formatter.parsedToCompute(file['Global KPI'][kpi].Countries)
-            let IPQ = await compute.computeIPQ(parsedData)
-            let ETM = await compute.computeETM(IPQ)
-            let ETMIPQ = await compute.computeETMIPQ(ETM)
-            let data = await formatter.dataToOut(ETMIPQ)
+            console.log(kpi,)
+            let parsedData = formatter.parsedToCompute(file['Global KPI'][kpi].Countries)
+            let data = compute.computeIPQ(parsedData)
+            data = compute.computeETM(data)
+            data = compute.computeETMIPQ(data)
+            data = formatter.dataToOut(data)
+
+            
 
             res.status(200).send({ success: true, data: data});
         } catch(e){
